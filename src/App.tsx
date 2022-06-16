ca19b207412b396086fa6a2210ded804aef7ce0b
Icaro Melchior <icaro_barbosa@outlook.com>
2022-06-16 01:37:39 -0300

    inicial

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Hero from './components/Hero'
+import NavBar from './components/NavBar';
+
+const App = () => {
+  return (
+   <div className='m-auto antaliset font-sans  bg-black text-white'>
+      <Hero />
+      <NavBar/>
+   </div>
+  );
+}
+
+export default App;
